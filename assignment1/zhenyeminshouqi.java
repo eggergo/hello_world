7a1a7a48a528def02c44ee59296d10354cd9fd08
egginbra <215931835@qq.com>
2016-09-11 15:42:16 +0800

    Update zhenyeminshouqi.java

--- assignment1/zhenyeminshouqi.java
+++ assignment1/zhenyeminshouqi.java
@@ -1,15 +1,24 @@
-struct proc {
-  uint sz;                     // Size of process memory (bytes)
-  pde_t* pgdir;                // Page table
-  char *kstack;                // Bottom of kernel stack for this process
-  enum procstate state;        // Process state
-  int pid;                     // Process ID
-  struct proc *parent;         // Parent process
-  struct trapframe *tf;        // Trap frame for current syscall
-  struct context *context;     // swtch() here to run process
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
-};
+import java.util.Scanner;  
+import java.text.DecimalFormat;  
+public class zhenyeshouqizhuang {  
+	public static void main(String[] args){  
+		System.out.println("welcome to zhenyeshouqizhuang");  
+		Scanner s1=new Scanner(System.in);  
+		System.out.println("please input the total money, use yuan as unit");  
+		double money=s1.nextDouble();  
+		Scanner s2=new Scanner(System.in);  
+		System.out.println("please input the total redbag number");  
+		int num=s2.nextInt();  
+		DecimalFormat df=new DecimalFormat("#####0.00");//stay %.2f  
+		int i;  
+		money-=num*0.01;//to make sure each redbag has at least 0.01 yuan  
+		for(i=num;i>1;i--){  
+			double a1=Math.random()*money;//to make a random  
+			money-=a1;  
+		  System.out.println("the "+ (num-i+1)+" redbag's money is "+df.format(a1+0.01)+" yuan");  
+		}  
+		System.out.println("the last redbag's money is "+df.format(money+0.01)+ "yuan");//the left money  
+	}  
+}  
+
+
